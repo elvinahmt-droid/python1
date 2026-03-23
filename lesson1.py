9d0667ed57cb6c9dcdca450fb0ed7c189a6d0c90
Elvin <elvinahmed@GOATs-MacBook-Air.local>
2026-03-23 15:37:01 +0400

    lesson1

---
+++ lesson1.py
@@ -0,0 +1,16 @@
+firstNumber=float(input('Enter first number: '))
+secondNumber=float(input('Enter second number: '))
+operation=str(input('Enter operation:(+-*/)'))
+if operation=='+':
+    print(firstNumber+secondNumber)
+elif operation=='-':
+    print(firstNumber-secondNumber)
+elif operation=='/':
+    if secondNumber != 0:
+        print(firstNumber/secondNumber)
+    else:
+        print("invalid input")
+elif operation=='*':
+    print(firstNumber*secondNumber)
+else:
+    print('Invalid operation')
